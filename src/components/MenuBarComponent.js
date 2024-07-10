db60a6d358d4be05ea856a42a4bc6948533b4efe
FlorianZasada <florian.zasada@gmail.com>
2024-07-10 12:40:43 +0200

    Fix menubar background

--- src/components/MenuBarComponent.js
+++ src/components/MenuBarComponent.js
@@ -6,7 +6,7 @@ import './MenuBarComponent.css'
 
 function MenuBarComponent({ backgroundColor, textColor}) {
   return (
-    <div className="nav-bar">
+    <div className="nav-bar" style={{backgroundColor: backgroundColor}}>
         <Link className='menu-btn' to="/" style={{color: textColor}}>Home</Link>
         <Link className='menu-btn' to="/about" style={{color: textColor}}>About</Link>
         <Link className='menu-btn' style={{backgroundColor: colors.primary, color: colors.white, border: '2px solid white'}} to="mailto:wiktoriazemla@gmail.com">Contact me</Link>
